65757033193ef73089e11186657415938eb89bac
Israel Colín <israaacolin@gmail.com>
2025-12-17 01:08:03 -0600

    Finalizando crud de transacciones, reporte mensual, comparativo y mejorando UI general

---
+++ Models/MonthlyReportViewModel.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using ControlFinancieroProject.Models;
+
+namespace ControlFinancieroProject.Models
+{
+    public class MonthlyReportViewModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+
+        public IEnumerable<Transaccion> Transacciones { get; set; } = new List<Transaccion>();
+
+        public List<string> DailyLabels { get; set; } = new List<string>();
+        public List<decimal> DailyExpenses { get; set; } = new List<decimal>();
+
+        public List<string> ExpenseCategoryLabels { get; set; } = new List<string>();
+        public List<decimal> ExpenseCategoryValues { get; set; } = new List<decimal>();
+
+        public List<string> IncomeCategoryLabels { get; set; } = new List<string>();
+        public List<decimal> IncomeCategoryValues { get; set; } = new List<decimal>();
+
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpense { get; set; }
+
+        public decimal CurrentBalance { get; set; }
+    }
+}
